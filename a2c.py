3079dea715a1da6bc259b5b74c6f39fc88f90277
Marc Aurel Vischer <vischer@campus.tu-berlin.de>
2020-03-12 16:17:32 +0100

    initial commit

---
+++ a2c.py
@@ -0,0 +1,220 @@
+"""
+Adapted from https://github.com/MorvanZhou/pytorch-A3C/blob/master/discrete_A3C.py
+"""
+import torch
+import torch.multiprocessing as mp
+import gym
+from DRRL.attention_module import DRRLnet
+from torch.distributions import Categorical
+import os
+import matplotlib.pyplot as plt
+
+
+torch.manual_seed(123)
+
+ENV_CONFIG = {"n": 8,
+              "goal_length": 3,
+              "num_distractor": 2,
+              "distractor_length": 2}
+
+env = gym.make('gym_boxworld:boxworld-v0', **ENV_CONFIG)
+env.seed(1)
+g_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+N_W = 3# mp.cpu_count()
+#todo: schedule workers to cpus
+N_ACT = env.action_space.n
+INP_W = env.observation_space.shape[0]
+INP_H = env.observation_space.shape[1]
+
+GAMMA = 0.99 #TODO: CHECK THIS
+SAVEPATH = os.path.join("." ,"save")
+os.mkdir(SAVEPATH)
+
+# #todo: check whether we really need this?
+# class SharedAdam(torch.optim.Adam):
+#     """Shared optimizer, the parameters in the optimizer will be shared across multiprocessors."""
+#     def __init__(self, params, lr=1e-3, betas=(0.9, 0.9), eps=1e-8, #todo: defaults 0.99
+#                  weight_decay=0):
+#         super(SharedAdam, self).__init__(params, lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
+#         # State initialization
+#         for group in self.param_groups:
+#             for p in group['params']:
+#                 state = self.state[p]
+#                 state['step'] = 0
+#                 state['exp_avg'] = torch.zeros_like(p.data)
+#                 state['exp_avg_sq'] = torch.zeros_like(p.data)
+#
+#                 # share in memory
+#                 state['exp_avg'].share_memory_()
+#                 state['exp_avg_sq'].share_memory_()
+
+
+class Worker(mp.Process):
+    def __init__(self, gnet, w_idx, device=g_device): #todo: distribute
+        """
+            Args:
+                gnet:   global network that performs parameter updates
+                w_idx:  integer index of worker process for identification
+                device: assigned device
+        """
+        super(Worker, self).__init__()
+        self.name = f"w{w_idx}"
+        self.g_net = gnet
+        self.l_net = DRRLnet(INP_W, INP_H, N_ACT).to(device)  # local network
+        self.env = gym.make('gym_boxworld:boxworld-v0', **ENV_CONFIG)
+        self.device = g_device
+
+    def start(self):
+        """Runs an entire episode, in the end return trajectory as list of s,a,r.
+
+        Returns:
+            Ready-to-digest trajectory as torch.tensors of state, action, reward+discounted future rewards
+        """
+        s = self.env.reset()
+        s_, a_, r_ = [], [], [] #trajectory of episode goes here
+        ep_r = 0. #total episode reward
+        ep_t = 0  #episode step t, both just for oversight
+        while True: #generate variable-length trajectory in this loop
+            s = torch.tensor([s.T], dtype=torch.float, device=self.device)  # transpose for CWH-order, apparently
+            # conv layer want floats
+            p, _ = self.l_net(s)
+            m = Categorical(p) # create a categorical distribution over the list of probabilities of actions
+            a = m.sample().item() # and sample an action using the distribution
+            s_new, r, done, _ = self.env.step(a)
+            ep_r += r
+
+            # append current step's elements to lists
+            s_.append(s)
+            a_.append(a)
+            r_.append(r)
+
+            if done:  # return trajectory as lists of elements
+                print(f"{self.name}: episode ended after step {ep_t} with total reward {ep_r}")
+                return(self.prettify_trajectory(s_, a_, r_))
+            s = s_new
+            ep_t += 1
+
+    def prettify_trajectory(self, s_, a_, r_):
+        """Prepares trajectory to be sent to central learner in a more orderly fashion.
+
+        Calculate temporally discounted future rewards for TD error and reverse lists so everything is in correct
+        temporal order. Cast everything to appropriate tensorflow tensors. Return what's necessary to compute
+        gradients upstream.
+
+        Args:
+            s_: List of states
+            a_: List of actions
+            r_: List of returns
+
+        Returns:
+            Ready-to-digest trajectory as torch.tensors of state, action, reward+discounted future rewards
+
+        """
+        # calculate temporal-discounted rewards
+        r_acc = 0
+        r_disc = []
+        for r in r_[::-1]:  # reverse buffer r, discount accordingly and add in value at for t=t_end
+            r_acc = r + GAMMA * r_acc
+            r_disc.append(r_acc)  # discounted trajectory in reverse order
+        r_disc = r_disc[::-1]
+        # every element in r_disc now contains reward at corresponding step plus future discounted rewards
+
+        #cast everything to tensors (states are already cast)
+        s_ = torch.cat(s_)
+        a_ = torch.tensor(a_, dtype=torch.uint8, device=self.device)
+        r_disc = torch.tensor(r_disc, dtype=torch.float16, device=self.device)
+
+        return(s_,a_,r_disc)
+
+    def pull_params(self):
+        """Update own params from global network."""
+        self.l_net.load_state_dict(self.g_net.state_dict(), strict=True)
+
+
+def update_step(net, trajectories, opt, opt_step):
+    """Calculate advantage-actor-critic loss on batch with network, updates parameters
+    Args:
+        net: network to perform training on
+        batch: list of trajectories as tuple of 1 tensor object for each
+            b_s:    states
+            b_a:    chosen actions
+            b_r_disc: returns and discounted future rewards for TD error with value function
+    Returns: Sum of loss of all trajectories
+    """
+    rezip = zip(*trajectories)
+    b_s, b_a, b_r_disc = [torch.cat(elems) for elems in list(rezip)] #concatenate torch tensors of all trajectories
+
+    net.train()  # sets net in training mode
+    b_p, b_v = net.forward(b_s)
+    # critic loss
+    td = b_r_disc - b_v
+    c_loss = td.pow(2)
+    #actor loss
+    m = torch.distributions.Categorical(b_p)
+    a_loss = - m.log_prob(b_a) * td.detach().squeeze()
+    #entropy term
+    e_loss = m.entropy()
+    e_w = min(1, 2*0.995**opt_step)
+    total_loss = (0.5*c_loss + a_loss + e_w*e_loss).mean() #todo: entropy annealing! #what happens without mean?
+    #global network: zero gradients, back-propagate loss, update params
+    opt.zero_grad()
+    total_loss.backward()
+    opt.step()
+    return total_loss.sum()
+
+def save_step(i_step, g_net, steps, losses):
+    try:
+        ending = f"{i_step:05}.pickle"
+        torch.save(g_net.state_dict(), os.path.join(SAVEPATH, "g_net"+ending))
+        torch.save(steps, os.path.join(SAVEPATH, "steps"+ending))
+        torch.save(losses, os.path.join(SAVEPATH, "losses"+ending))
+    except Exception as e:
+        print(f"failed to write step {i_step} to disk:")
+        print(e)
+    try:
+        # clean out old files
+        oldfiles = [f for f in os.listdir(SAVEPATH)
+                    if (f.startswith("g_net") or f.startswith("steps") or f.startswith("losses"))
+                    and not f.endswith(ending)]
+        for f in oldfiles:
+            os.remove(os.path.join(SAVEPATH, f))
+    except Exception as e:
+        print("failed to erase old saves:")
+        print(e)
+
+def load_step(path):
+    pass
+    # todo: implement
+
+if __name__ == "__main__":
+    #create global network and pipeline
+    g_net = DRRLnet(INP_W, INP_H, N_ACT).to(g_device) # global network
+    g_net.share_memory()  # share the global parameters in multiprocessing
+    # optimizer = SharedAdam(g_net.parameters(), lr=0.0001)  # global optimizer
+    optimizer = torch.optim.Adam(g_net.parameters()) #todo: check parameters in paper
+    g_step = 0 # mp.Value('i', 0) #global step counter
+    # g_ep = mp.Value('i', 0) #global episode counter
+    # g_ep_r = mp.Value('d', 0.) #global reward counter
+    # res_queue = mp.Queue() #queue to push trajectories to
+    N_STEP = 10
+
+    #create workers
+    losses = []
+    steps = []
+    trajectories = []
+    workers = [Worker(g_net, i) for i in range(N_W)]
+    for i_step in range(N_STEP): #performing one parallel update step
+        #parallel trajectory sampling
+        trajectories = [w.start() for w in workers] #list comprehension automatically waits for workers to finish
+        #concatenate and push tracjectories to global network for learning (synchronized update)
+        loss = update_step(g_net, trajectories, optimizer, i_step)
+        #pull new parameters
+        [w.pull_params() for w in workers]
+        #bookkeeping
+        g_step += sum([len(traj[0])for traj in trajectories])
+        steps.append(g_step)
+        losses.append(loss)
+        if i_step%1 == 0: #save global network
+            save_step(i_step, g_net, steps, losses)
+
+    plt.plot(steps, losses)
---
+++ attention_module.py
@@ -0,0 +1,176 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import math
+
+import torch.optim
+import torch.autograd
+# from collections import namedtuple
+
+# Transition = namedtuple('Transition',
+#                         ('state', 'action', 'next_state', 'reward'))
+
+# class ReplayMemory(object):
+#
+#     def __init__(self, capacity):
+#         self.capacity = capacity
+#         self.memory = []
+#         self.position = 0
+#
+#     def push(self, *args):
+#         """Saves a transition."""
+#         if len(self.memory) < self.capacity:
+#             self.memory.append(None)
+#         self.memory[self.position] = Transition(*args)
+#         self.position = (self.position + 1) % self.capacity
+#
+#     def sample(self, batch_size):
+#         return random.sample(self.memory, batch_size)
+#
+#     def __len__(self):
+#         return len(self.memory)
+
+class AttentionHead(nn.Module):
+
+    def __init__(self, n_elems, elem_size, emb_size):
+        super(AttentionHead, self).__init__()
+        self.sqrt_emb_size = int(math.sqrt(emb_size))
+        #queries, keys, values
+        self.query = nn.Linear(elem_size, emb_size)
+        self.qln = nn.LayerNorm(emb_size, elementwise_affine=False)
+        # From 2018 version of paper it sounds like individual projected
+        # entities q_i, k_i, v_i are 0,1 normalized (layer norm). However it's unclear whether and how gain and bias
+        # are learned, e.g. one gain and bias for each projected entity, or for each element in the vectors of each
+        # entity (and whether these are shared), or not at all?
+        # For now I decided to not use an affine transform in the end.
+        self.key = nn.Linear(elem_size, emb_size)
+        self.kln = nn.LayerNorm(emb_size, elementwise_affine=False)
+        self.value = nn.Linear(elem_size, elem_size)
+        self.vln = nn.LayerNorm(elem_size, elementwise_affine=False)
+        self.softmax = nn.Softmax(dim=0) #hope dim=0 is correct
+
+    def forward(self, x):
+        Q = self.qln(self.query(x))
+        K = self.kln(self.key(x))
+        V = self.vln(self.value(x))
+        return torch.bmm(self.softmax(torch.bmm(Q,K.transpose(1,2))/self.sqrt_emb_size),V)
+
+
+class AttentionModule(nn.Module):
+
+    def __init__(self, n_elems, elem_size, emb_size, n_heads):
+        super(AttentionModule, self).__init__()
+        # self.input_shape = input_shape
+        # self.elem_size = elem_size
+        # self.emb_size = emb_size #honestly not really needed
+        self.heads = [AttentionHead(n_elems, elem_size, emb_size) for _ in range(n_heads)]
+        self.linear1 = nn.Linear(n_heads*elem_size, elem_size)
+        self.linear2 = nn.Linear(elem_size, elem_size)
+
+        self.ln = nn.LayerNorm(elem_size, elementwise_affine=True)
+
+    def forward(self, x):
+        #concatenate all heads' outputs
+        A_cat = torch.cat([head(x) for head in self.heads], -1)
+        # projecting down to original element size with 2-layer MLP, layer size = entity size
+        mlp_out = F.relu(self.linear2(F.relu(self.linear1(A_cat))))
+        # residual connection and final layer normalization
+        return self.ln(x + mlp_out)
+
+
+class DRRLnet(nn.Module):
+
+    def __init__(self, h, w, outputs, att_emb_size=64, n_heads=2, n_att_stack=2, pad=True):
+
+        #internal action replay buffer for simple training algorithms
+        self.saved_actions = []
+        self.rewards = []
+
+        self.pad = pad
+
+        super(DRRLnet, self).__init__()
+
+        n_f_conv1 = 12
+        n_f_conv2 = 24
+
+        self.conv1 = nn.Conv2d(3, n_f_conv1, kernel_size=2, stride=1)
+        #possibly batch or layer norm, neither was mentioned in the paper though
+        # self.ln1 = nn.LayerNorm([n_f_conv1,conv1w,conv1h])
+        # self.bn1 = nn.BatchNorm2d(n_f_conv1)
+        self.conv2 = nn.Conv2d(n_f_conv1, n_f_conv2, kernel_size=2, stride=1)
+        # self.ln2 = nn.LayerNorm([n_f_conv2,conv2w,conv2h])
+        # self.bn2 = nn.BatchNorm2d(n_f_conv2)
+
+        # calculate size of convolution module output
+        def conv2d_size_out(size, kernel_size=2, stride=1):
+            return (size - (kernel_size - 1) - 1) // stride + 1
+        if self.pad:
+            conv1w = conv2w = w
+            conv1h = conv2h = h
+        else:
+            conv1w = conv2d_size_out(w)
+            conv1h = conv2d_size_out(h)
+            conv2w = conv2d_size_out(conv1w)
+            conv2h = conv2d_size_out(conv1h)
+
+        # create x,y coordinate matrices to append to convolution output
+        xmap = np.linspace(-np.ones(conv2h), np.ones(conv2h), num=conv2w, endpoint=True, axis=0)
+        xmap = torch.tensor(np.expand_dims(np.expand_dims(xmap,0),0), dtype=torch.float32)
+        ymap = np.linspace(-np.ones(conv2w), np.ones(conv2w), num=conv2h, endpoint=True, axis=1)
+        ymap = torch.tensor(np.expand_dims(np.expand_dims(ymap,0),0), dtype=torch.float32)
+        self.xymap = torch.cat((xmap,ymap),dim=1) # shape (1, 2, conv2w, conv2h)
+
+        #create attention module with n_heads heads and remember how many times to stack it
+        self.n_att_stack = n_att_stack #how many times the attentional module is to be stacked (weight-sharing -> reuse)
+        att_elem_size = n_f_conv2 + 2 #an "attendable" entity has 24 CNN channels + 2 coordinate channels = 26 features
+        self.attMod = AttentionModule(conv2w*conv2h, att_elem_size, att_emb_size, n_heads)
+
+        #max pooling
+        # print(f"attnl element size:{att_elem_size}")
+        # self.maxpool = nn.MaxPool1d(kernel_size=att_emb_size,return_indices=False) #don't know why maxpool reduces
+        # kernel_size by 1
+
+        # 4 FC256 layers
+        self.fc1 = nn.Linear(att_elem_size, 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc3 = nn.Linear(256, 256)
+        self.fc4 = nn.Linear(256, 256)
+        self.logits = nn.Linear(256, outputs)
+        self.value = nn.Linear(256, 1)
+
+    # Called with either one element to determine next action, or a batch
+    # during optimization. Returns tensor([[left0exp,right0exp]...]).
+    def forward(self, x):
+        #convolutional module
+        if self.pad:
+            x = F.pad(x, (1,0,1,0)) #zero padding so state size stays constant
+        c = F.relu(self.conv1(x))
+        if self.pad:
+            c = F.pad(c, (1,0,1,0))
+        c = F.relu(self.conv2(c))
+        #append x,y coordinates to every sample in batch
+        batchsize = c.size(0)
+        batch_maps = torch.cat(batchsize*[self.xymap])
+        c = torch.cat((c,batch_maps),1)
+        #attentional module
+        #careful: we are flattening out x,y dimensions into 1 dimension, so shape changes from (batchsize, #filters,
+        # #conv2w, conv2h) to (batchsize, conv2w*conv2h, #filters), because downstream linear layers take last
+        # dimension to be input features
+        a = c.view(c.size(0),c.size(1), -1).transpose(1,2)
+        # n_att_mod passes through attentional module -> n_att_mod stacked modules with weight sharing
+        for i_att in range(self.n_att_stack):
+            a = self.attMod(a)
+        #max pooling over "space", i.e. max scalar within each feature map m x n x f -> f
+        # pool over entity dimension #isn't this a problem with gradients?
+        # todo: try pooling over feature dimension
+        pooled = F.max_pool1d(a.transpose(1,2), kernel_size=a.size(1)) #pool out entity dimension
+        #policy module: 4xFC256, then project to logits and value
+        p = F.relu(self.fc1(pooled.view(pooled.size(0),pooled.size(1))))
+        p = F.relu(self.fc2(p))
+        p = F.relu(self.fc3(p))
+        p = F.relu(self.fc4(p))
+        pi = F.softmax(self.logits(p), dim=1)
+        v = self.value(p) #todo: no normalization?
+        return pi, v
+
+
